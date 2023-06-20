55cd59ab3bd28e96916ccc7d3004a479c2040ee9
david95j2 <glory20220j@gmail.com>
2023-06-20 19:33:59 +0900

    init project

---
+++ src/main/java/com/example/sierra/SierraApplication.java
@@ -0,0 +1,13 @@
+package com.example.sierra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SierraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SierraApplication.class, args);
+    }
+
+}
